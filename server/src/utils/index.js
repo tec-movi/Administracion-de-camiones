dfb73c16b49c374b9fe8d7e303cc050dbed8dfc5
BENJAMIN IGNACIO ALVAREZ CHOCANO <benjaminiach4@gmail.com>
2026-06-01 19:26:23 -0400

    Delete server directory

--- server/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const createHash = async (password) => {
-  const salts = await bcrypt.genSalt(10)
-  return bcrypt.hash(password, salts)
-}
-
-export const passwordValidation = async(user, password) => bcrypt.compare(password, user.password)
